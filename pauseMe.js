0f230819a4f079da38d44cfa38e24ef14dda20e1
L.Malinowski <lmalinowski@cyfrowypolsat.com>
2015-06-23 00:03:34 +0100

    pausing yt tabs works

--- pauseMe.js
+++ pauseMe.js
@@ -1,18 +1,24 @@
 console.log('ver 0.1.1')
 
 function hello() {
-    var qi = {
+    var qi =  {
         url: '*://*.youtube.com/*'
     }
 
-    chrome.tabs.query(qi, function(tabs) {
+    chrome.tabs.query(qi, function (tabs) {
         console.log("tabs.length: " + tabs.length)
-        tabs.forEach(function(t){
-            chrome.tabs.highlight({windowId: tabs.windowId, tabs: t.index}, function(w) {})
+        //tabs.forEach(function(t){
+        //    chrome.tabs.highlight({windowId: tabs.windowId, tabs: t.index}, function(w) {})
+        //})
+
+        tabs.forEach(function (tab) {
+            chrome.tabs.sendMessage(tab.id, {cmd: "pause"}, function (response) {
+                console.log(response);
+            });
         })
     })
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function (tab) {
     hello();
 });
