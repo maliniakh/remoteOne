51462ccea51c0a64f646a961ff43459ede7d0bbf
L.Malinowski <lmalinowski@cyfrowypolsat.com>
2015-06-23 00:03:34 +0100

    rebase -i?

--- content-script.js
+++ content-script.js
@@ -3,6 +3,34 @@
 //
 //})
 
+<<<<<<< HEAD
+=======
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+        "from a content script:" + sender.tab.url : "from the extension");
+        //if (request. == "hello")
+        //    sendResponse({farewell: "goodbye"});
+
+        //$(".ytp-button").click(function () {
+        //    //alert("test")
+        //    console.log("click " + $(this));
+        //})
+
+        var btn = $(".ytp-button.ytp-button-play");
+        if(btn.length == 1) {
+            btn.trigger('click');
+        } else {
+            btn = $(".ytp-button.ytp-button-pause");
+            if(btn.length == 1) {
+                btn.trigger('click');
+            }
+        }
+
+        console.log("clicked " + btn);
+    });
+>>>>>>> 487e657... pausing yt tabs works, autor zmieniony
 
 
 chrome.runtime.onMessage.addListener(
