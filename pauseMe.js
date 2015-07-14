b9bc83487b633febd6a9ceef6c64607ab44c6f73
L.Malinowski <maliniakh@gmail.com>
2015-07-14 15:35:50 +0100

    adding controls for each relevant tab opened

--- pauseMe.js
+++ pauseMe.js
@@ -2,8 +2,6 @@
 
 var pausedTabs = undefined;
 
-var sitesUrls = ['*://*.youtube.com/*', '*://soundcloud.com/*'];
-
 function execute() {
     // todo: pause or play?
     var cmd = 'pause';
@@ -31,24 +29,9 @@ function execute() {
 }
 
 chrome.browserAction.onClicked.addListener(function (tab) {
+    alert('browser action')
     execute();
 });
 
 
-function getRelevantTabs() {
-    var result = [];
 
-    sitesUrls.forEach(function (url) {
-        var qi = {
-            url: url
-        };
-
-        chrome.tabs.query(qi, function (tabs) {
-            console.log("tabs for " + url + ": " + tabs.length);
-            result = $.merge(result, tabs);
-        });
-    });
-
-    console.log("all relevant tabs: " + result.length);
-    return result;
-}
--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
-//alert('chuj')
+"use strict"
+
+var sitesUrls = ['*://*.youtube.com/*', '*://soundcloud.com/*'];
+var result = [];
+
+
+
 document.addEventListener('DOMContentLoaded', function () {
+    getRelevantTabs(addControls);
+
     document.getElementById('prev').onclick = function () {
         alert('prev');
     };
@@ -16,6 +24,47 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('next');
     };
 });
+
+// make it return tabs actually
+function getRelevantTabs(callback) {
+    sitesUrls.forEach(function (url) {
+        var qi = {
+            url: url
+        };
+
+        chrome.tabs.query(qi, function (tabs) {
+            console.log("tabs for " + url + ": " + tabs.length);
+            result = $.merge(result, tabs);
+            addControls(tabs)
+        });
+    });
+
+    console.log("all relevant tabs: " + result.length);
+    //return result;
+}
+
+function addControls(tabs) {
+    if(tabs.length == 0) {
+        return;
+    }
+
+    for(var i = 0; i < tabs.length; i++) {
+        var tab = tabs[i];
+
+        // id, visibility
+        var templateDiv = $('#template');
+        var controlsDiv = templateDiv.clone(false);
+        controlsDiv.attr('id', 'tab_' + tab.windowId + "_" + tab.id);
+        controlsDiv.css('visibility', 'visible');
+
+        // title
+        controlsDiv.find('.title').text(tab.title);
+
+        templateDiv.after(controlsDiv);
+    }
+
+}
+
 //$("#next").click(function () {
 //    alert('next')
 //});
